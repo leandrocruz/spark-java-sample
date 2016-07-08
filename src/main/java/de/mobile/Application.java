c12dc4d4412f2d4c6464ab84841eb2442c97bf28
Leandro Cruz <leandro.saad@gmail.com>
2016-07-08 12:42:49 +0200

    Minor refactoring

--- src/main/java/de/mobile/Application.java
+++ src/main/java/de/mobile/Application.java
@@ -2,53 +2,40 @@ package de.mobile;
 
 import static spark.Spark.get;
 
-import org.apache.avalon.framework.activity.Initializable;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import de.mobile.codec.Codec;
 import de.mobile.domain.ImmutableSample;
 import spark.Request;
 import spark.Response;
-import spark.Route;
-import xingu.container.Container;
 import xingu.container.ContainerUtils;
-import xingu.container.Inject;
 import xingu.factory.Factory;
 import xingu.lang.NotImplementedYet;
 
 public class Application
-	implements Initializable
+	extends ApplicationSupport
 {
-	@Inject
-	private Codec codec;
-	
-	private final Logger logger = LoggerFactory.getLogger(Application.class);
-	
 	public static void main(String[] args)
 		throws Exception
 	{
-		final Container container = ContainerUtils.getContainer();
-		final Factory factory = container.lookup(Factory.class);
-		factory.create(Application.class);
+		ContainerUtils
+			.getContainer()
+			.lookup(Factory.class)
+			.create(Application.class);
 	}
 
 	@Override
-	public void initialize()
+	protected void registerRoutes()
 		throws Exception
 	{
-		get("/customer/:name/:age", new RouteHandler((req, res) -> onOk(req, res)));
-		get("/err", 				new RouteHandler((req, res) -> onError(req, res)));
-
+		get("/customer/:name/:age", handle((req, res) -> onOk(req, res)));
+		get("/err", 				handle((req, res) -> onError(req, res)));	
 	}
 
-	private Object onError(Request req, Response res)
+	Object onError(Request req, Response res)
 		throws Exception
 	{
 		throw new NotImplementedYet("sorry!");
 	}
 
-	private Object onOk(Request request, Response response)
+	Object onOk(Request request, Response response)
 		throws Exception
 	{
 		final String age  = request.params(":age");
@@ -60,39 +47,5 @@ public class Application
 				.age(Integer.parseInt(age))
 				.addFavoriteColors("blue", "gray", "green")
 				.build();
-	}
-
-	class RouteHandler
-		implements Route
-	{
-		private final Route route;
-
-		public RouteHandler(Route route)
-		{
-			this.route = route;
-		}
-		
-		@Override
-		public Object handle(Request request, Response response)
-			throws Exception
-		{
-			final boolean allowed = true; //authManager.isAllowed(request, response);
-			if(!allowed)
-			{
-				response.status(401);
-				return "Request rejected";
-			}
-			try
-			{
-				final Object result = route.handle(request, response);
-				return codec.encode(result);
-			}
-			catch(Exception e)
-			{
-				logger.error("Error handling request", e);
-				response.status(500);
-				return "Error handling request: " + e.getMessage(); //maybe report the exception to the client?
-			}
-		}
 	}
 }
---
+++ src/main/java/de/mobile/ApplicationSupport.java
@@ -0,0 +1,73 @@
+package de.mobile;
+
+import org.apache.avalon.framework.activity.Initializable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.mobile.codec.Codec;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import xingu.container.Inject;
+
+import static spark.Spark.port;
+
+public abstract class ApplicationSupport
+	implements Initializable
+{
+	@Inject
+	protected Codec codec;
+	
+	private final Logger logger = LoggerFactory.getLogger(Application.class);
+
+	@Override
+	public void initialize()
+		throws Exception
+	{
+		final int listenAt = 8000;
+		port(listenAt);
+		registerRoutes();
+	}
+
+	protected abstract void registerRoutes()
+		throws Exception;
+
+	protected Route handle(Route route)
+	{
+		return new RouteHandler(route);
+	}
+
+	private class RouteHandler
+		implements Route
+	{
+		private final Route route;
+	
+		public RouteHandler(Route route)
+		{
+			this.route = route;
+		}
+		
+		@Override
+		public Object handle(Request request, Response response)
+			throws Exception
+		{
+			final boolean allowed = true; //authManager.isAllowed(request, response);
+			if(!allowed)
+			{
+				response.status(401);
+				return "Request rejected";
+			}
+			try
+			{
+				final Object result = route.handle(request, response);
+				return codec.encode(result);
+			}
+			catch(Exception e)
+			{
+				logger.error("Error handling request", e);
+				response.status(500);
+				return "Error handling request: " + e.getMessage(); //maybe report the exception to the client?
+			}
+		}
+	}
+}
